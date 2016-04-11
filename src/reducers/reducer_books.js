e14fbe4b4b5786ce2a6483395dba6fa689e87a59
Coderbit <sashayu@me.com>
2016-04-11 16:00:24 +0300

    action creator and active book placeholder

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,7 +3,7 @@ export default function () {
     {title: 'JS Ninja'},
     {title: 'How to code'},
     {title: 'ES6 the new parts'},
-    {title: 'About hot not to procrastinate'},
+    {title: 'About how not to procrastinate'},
     {title: 'The Beginning of JS'}
   ]
 }
