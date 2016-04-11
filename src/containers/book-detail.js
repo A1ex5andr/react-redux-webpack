12050d693da677344149efeda984dab39158816a
Coderbit <sashayu@me.com>
2016-04-11 16:25:28 +0300

    book selected

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,9 +1,32 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class BookDetail extends Component {
+
+class BookDetail extends Component {
   render() {
+    if (!this.props.book)
+      return <div className="label label-default">Please, select book to get started</div>
+
     return (
-      <div>Book Detail</div>
-    )
+      <div className="col-sm-8">
+        <h3>Details for:</h3>
+        <br/>
+        <div>
+          Title:
+          <b> {this.props.book.title} </b>
+        </div>
+        <div>Pages:
+          <i> {this.props.book.pages}</i>
+        </div>
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function () {
   return [
-    {title: 'JS Ninja'},
-    {title: 'How to code'},
-    {title: 'ES6 the new parts'},
-    {title: 'About how not to procrastinate'},
-    {title: 'The Beginning of JS'}
+    {title: 'JS Ninja', pages: '213'},
+    {title: 'How to code', pages: '98'},
+    {title: 'ES6 the new parts', pages: '139'},
+    {title: 'About how not to procrastinate', pages: '1'},
+    {title: 'The Beginning of JS', pages: '874'}
   ]
 }
